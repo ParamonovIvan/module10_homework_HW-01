8bbedbbae3d27c5e276a8be7b49e3cc453114dc7
ParamonovIvan <131868856+ParamonovIvan@users.noreply.github.com>
2023-05-19 16:14:19 +0400

    Delete Задание1.js

--- Задание1.js
+++
@@ -1,8 +0,0 @@
-const result = +prompt('¬ведите число', '')
-if (isNaN(result % 2)) {
-  alert('”пс, кажетс€, вы ошиблись')
-} else if (result % 2 === 0) {
-  alert('„исло четное')
-} else {
-  alert('„исло нечетное')
-}
