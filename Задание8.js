52a7eaa578b4f88051d904ec6b6e79c5f2789a1d
ParamonovIvan <131868856+ParamonovIvan@users.noreply.github.com>
2023-05-19 16:15:13 +0400

    Delete Задание8.js

--- Задание8.js
+++
@@ -1,9 +0,0 @@
-let vegetable = new Map([
-  ["potato", "brown"],
-  ["carrot", "orange"],
-  ["tomato", "red"],
-  ["peas", "green"],
-]);
-for (let [key, value] of vegetable) {
-  console.log(`Ключ - ${key}, значение - ${value}`);
-}
